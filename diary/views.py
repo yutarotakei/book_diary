e0900cabd170fda3523fa61a3915caac08724a41
YutaroTakei <yutarotakei@icloud.com>
2021-08-25 16:10:14 +0900

    6th commit

--- diary/views.py
+++ diary/views.py
@@ -63,11 +63,6 @@ def bookcreate(request, *args, **kwargs):
     return render(request, 'book_create.html', {'form': form})
 
 
-
-# グラフ作成
-
-
-
 # SVG化
 def plt2svg():
     buf = io.BytesIO()
@@ -79,8 +74,12 @@ def plt2svg():
 
 # 実行するビュー関数
 def get_svg(request):
-    x = ["1", "2", "3", "4", "5", "6", "7", "8", "9","10", "11", "12"]
-    y = [3, 5, 0, 5, 6, 10, 2, 3, 4, 7, 4, 5]
+    y = []
+    for i in range(1, 13):
+        m1 = Book.objects.filter(user=request.user).filter(read_at__year='2021', read_at__month='{}'.format(i)).count()
+        y.append(m1)
+    x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    plt.rcParams["figure.figsize"] = (8, 6)
     plt.bar(x, y, color='#00d5ff')
     plt.title("the number of books you read")
     plt.xlabel("Month")
